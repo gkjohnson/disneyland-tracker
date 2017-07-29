de5ada89d5bb6a0e4c2fec7796137c489b8322e8
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-07-29 14:25:46 -0700

    Add basic wait time tracker

---
+++ index.js
@@ -0,0 +1,29 @@
+const ThemeParks = require('themeparks')
+
+const park = new ThemeParks.Parks.DisneylandResortMagicKingdom()
+const lastTimes = {}
+
+const pad = (str, char, width) => {
+    if (str.length >= width) return str
+
+    return pad(str + char, char, width)
+}
+
+const _do = () => {
+    park.GetWaitTimes(function(err, rides) {
+        rides.forEach(ride => {
+            if (lastTimes[ride.id] && lastTimes[ride.id].lastUpdate !== ride.lastUpdate) {
+                const delta = ride.lastUpdate - lastTimes[ride.id].lastUpdate
+                console.log(`${pad(ride.name, ' ', 65)} from ${lastTimes[ride.id].waitTime} to ${ride.waitTime}min\t\tafter ${delta}ms`)
+            } else if (!lastTimes[ride.id]) {
+                console.log(`${pad(ride.name, ' ', 65)} wait is ${ride.waitTime}min`)
+            }
+
+            lastTimes[ride.id] = ride
+        })
+
+        setTimeout(_do, 10 * 1e3)
+    })
+}
+
+_do()
